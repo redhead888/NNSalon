6e0531cab5beec15ec824b91b7798a6eae4c5b1e
redhead888 <121343348+redhead888@users.noreply.github.com>
2024-06-05 13:59:52 +0300

    Update php.php

--- php.php
+++ php.php
@@ -1,18 +1,16 @@
 <?php
-$servername = "localhost"; // имя сервера MySQL
-$username = ""; // имя пользователя MySQL
-$password = ""; // пароль пользователя MySQL
-$dbname = "mydb"; // имя вашей базы данных
+$servername = "localhost"; 
+$username = ""; 
+$password = "";
+$dbname = "mydb"; 
 
-// Создаем соединение
+
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Проверяем соединение
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-// Проверяем, что данные пришли из формы
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $name = $conn->real_escape_string($_POST['requestName']);
     $phone = $conn->real_escape_string($_POST['requestPhone']);
