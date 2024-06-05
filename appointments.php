73e6d300ac953d7f1c7ca2eab8283603c29c80af
redhead888 <121343348+redhead888@users.noreply.github.com>
2024-06-05 13:59:30 +0300

    Update appointments.php

--- appointments.php
+++ appointments.php
@@ -1,24 +1,20 @@
 <?php
-$servername = "localhost"; // имя сервера MySQL
-$username = "u2597607_root"; // имя пользователя MySQL
-$password = "root12345678"; // пароль пользователя MySQL
-$dbname = "u2597607_root"; // имя вашей базы данных
+$servername = "localhost"; 
+$username = ""; 
+$password = ""; 
+$dbname = "mydb"; 
 
-// Создаем соединение
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Проверяем соединение
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-// Получаем данные из POST запроса
 $name = $_POST['name'];
 $phone = $_POST['phone'];
 $date = $_POST['date'];
 $time = $_POST['time'];
 
-// Добавляем данные в базу данных
 $sql = "INSERT INTO appointments (name, phone, date, time) VALUES ('$name', '$phone', '$date', '$time')";
 
 if ($conn->query($sql) === TRUE) {
